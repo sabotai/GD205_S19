0ab617384c0d547f82e04df53d18775b6ea8e9b3
Juno Morrow <sabotaione@gmail.com>
2019-02-13 16:23:34 -0500

    Basic board movement, goal, hazard

---
+++ Assets/Scripts/BoardMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMovement : MonoBehaviour {
+    Vector3 playerPos;
+    public Transform destination;
+    public Transform hazard;
+
+    // Use this for initialization
+    void Start () {
+        playerPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerPos += transform.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerPos -= transform.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerPos += transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerPos -= transform.right;
+        }
+
+        if (playerPos.x == destination.position.x && 
+            playerPos.z == destination.position.z){
+            playerPos += transform.up;
+        }
+
+        if (playerPos.x == hazard.position.x &&
+            playerPos.z == hazard.position.z)
+        {
+            playerPos -= transform.up;
+        }
+        transform.position = playerPos;
+    }
+}
