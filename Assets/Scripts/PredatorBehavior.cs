d0c3056d56c62495facd0ba513de1ca9324d9e7d
Juno Morrow <sabotaione@gmail.com>
2019-03-06 16:58:49 -0500

    added w6 stuff

---
+++ Assets/Scripts/PredatorBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredatorBehavior : MonoBehaviour {
+
+    Rigidbody predRB;
+    public Transform prey;
+    public float forceAmt;
+
+	// Use this for initialization
+	void Start () {
+        predRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 preyDirection = Vector3.Normalize(prey.position - transform.position);
+        predRB.AddForce(preyDirection * forceAmt);
+	}
+}
---
+++ Assets/Scripts/PreyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreyBehavior : MonoBehaviour {
+
+    Rigidbody preyRB;
+    public Transform pred;
+    public float forceAmt;
+    // Use this for initialization
+    void Start()
+    {
+        preyRB = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 predDirection = Vector3.Normalize(transform.position - pred.position);
+        preyRB.AddForce(predDirection * forceAmt);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform == pred){
+            Debug.Log("you hit me and you are a predator");
+
+        }
+    }
+
+}
