2a266957d99a91ba832b67c6df62d15b88e361a3
Juno Morrow <sabotaione@gmail.com>
2019-04-10 15:11:26 -0400

    animation week

---
+++ Assets/Scripts/ArcherAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherAnim : MonoBehaviour
+{
+    Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnim.SetInteger("state", 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            myAnim.SetInteger("state", 1);
+        }
+
+    }
+}
