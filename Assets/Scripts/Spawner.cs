1e85b42ae342f57db17277df1c8c86282824b9ba
Juno Morrow <sabotaione@gmail.com>
2019-04-10 16:43:08 -0400

    added week 11 material

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject clone;
+    public Vector3 spawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Instantiate(clone, spawnLocation, Quaternion.identity);
+
+        }
+    }
+}
