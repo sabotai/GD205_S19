353d593622e6d571f7045fa3dc999428dd0784f4
Juno Morrow <sabotaione@gmail.com>
2019-04-17 16:44:48 -0400

    added w12

---
+++ Assets/Scripts/UIDemo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIDemo : MonoBehaviour
+{
+
+    Text myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "" + Time.time;
+    }
+
+    public void ChangeText()
+    {
+        Debug.Log("you pressed the button");
+
+    }
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
