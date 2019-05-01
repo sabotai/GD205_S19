870b43adaee4e209fa1a22de23042b6312ce68d5
Juno Morrow <sabotaione@gmail.com>
2019-05-01 16:22:13 -0400

    nav mesh stuff

---
+++ Assets/Scripts/MyNavAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MyNavAgent : MonoBehaviour
+{
+
+    NavMeshAgent nav;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //nav.destination = target.position;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("your trigger hit... " + other.name);
+        if (other.gameObject.CompareTag("Prey"))
+        {
+            nav.destination = other.transform.position;
+        }
+    }
+}
